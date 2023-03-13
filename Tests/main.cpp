709d7c4a2a0fc915d97a6b742b3efd186bde5b71
Gonçalo Pinto <gongaspinto03@gmail.com>
2023-03-13 17:28:17 +0000

    created proj

---
+++ Tests/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gpinto03 on 13-03-2023.
+//
+
+#include "main.h"
---
+++ Tests/main.h
@@ -0,0 +1,14 @@
+//
+// Created by gpinto03 on 13-03-2023.
+//
+
+#ifndef PROJ1_MAIN_H
+#define PROJ1_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PROJ1_MAIN_H
